daefc21fa41464529e52a5ed790dabc7356190f5
Ana Inés Mardon <anitamardon@gmail.com>
2015-09-24 00:11:05 -0300

    armado de notas del estudiante

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -3,7 +3,20 @@ class MarksController < ApplicationController
 
   # GET /marks
   def index
-    #@marks = Mark.all
+    response = Faraday.get (Settings.api.url + '/students/5/school_years'), content_type: 'application/json'
+    res = JSON.parse response.body
+
+    if params[:school_year].nil?
+      @school_year = res['school_years'].last
+      @school_year = @school_year['year']
+    else
+      @school_year = params[:school_year]
+    end
+    response = Faraday.get (Settings.api.url + "/students/5/marks/#{@school_year}"), content_type: 'application/json'
+    res_marks = JSON.parse response.body
+
+    @school_years = res['school_years']
+    @marks = res_marks['marks']
   end
 
   # GET /marks/1
