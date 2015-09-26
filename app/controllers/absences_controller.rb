4d724e3aac40bed8a1f0846c1803d5a6c81b2ded
Ana Inés Mardon <anitamardon@gmail.com>
2015-09-26 13:23:41 -0300

    vista sansiones disciplinarias

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -2,7 +2,7 @@ class AbsencesController < ApplicationController
   # GET /absences
 
   def index
-    res = Api::call '/students/2003/school_years'
+    res = Api::call '/students/5/school_years'
 
     if params[:school_year].nil?
       @school_year = res['school_years'].last
@@ -11,9 +11,9 @@ class AbsencesController < ApplicationController
       @school_year = params[:school_year]
     end
 
-    res_absences = Api::call "/students/2003/absences/#{@school_year}"
+    res_absences = Api::call "/students/5/absences/#{@school_year}"
 
     @school_years = res['school_years']
-    @absences = res_absences['absences']
+    @absences = res_absences['absences_for_year']
   end
 end
